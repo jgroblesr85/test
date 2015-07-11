58f9926ce0833c7810f40ba4c6934c08562709fd
jgroblesr85 <jgroblesr85@gmail.com>
2015-07-10 23:10:19 -0500

    Filter the Notes List

---
+++ assets/js/filters/categoryFilter.js
@@ -0,0 +1,20 @@
+angular.module('NoteWrangler')
+  .filter('categoryFilter', function(){
+
+    return function(collection, category){
+        var newCollection=[];
+
+        if (category && category.id) {
+
+          for(var i=0, l=collection.length; i < l; i++) {
+            if(collection[i].category_id === category.id) {
+              newCollection.push(collection[i]);
+            }
+          }
+
+          return newCollection;
+        } else return collection;
+
+    }
+
+  });
